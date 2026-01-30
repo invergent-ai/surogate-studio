a030e72abb33b18f25c9e18df6da81d2cc51445f
dan.chirila <dan.chirila@m-ware.eu>
2026-01-30 10:49:29 +0200

    - demo.surogate.ai

--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   I18N_HASH: 'generated_hash',
-  SERVER_API_URL: 'https://studio.densemax.local',
+  SERVER_API_URL: 'https://demo.surogate.ai',
   VERSION: "1.0.0-prod",
   DEBUG_INFO_ENABLED: false,
   dev: false
